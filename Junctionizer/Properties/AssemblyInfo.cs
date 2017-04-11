7bc75cc900ee3f475ec4e40d03af5d98013ac1de
NickLargen <nick@elargen.com>
2017-04-11 18:02:08 -0500

    Remove unnecessary InternalsVisibleTo

--- Junctionizer/Properties/AssemblyInfo.cs
+++ Junctionizer/Properties/AssemblyInfo.cs
@@ -53,4 +53,3 @@ using System.Windows;
 [assembly: AssemblyVersion("0.0.0")]
 [assembly: AssemblyFileVersion("0.1.0")]
 [assembly: AssemblyInformationalVersion("Alpha")]
-[assembly: InternalsVisibleTo("Junctionizer.Tests")]
